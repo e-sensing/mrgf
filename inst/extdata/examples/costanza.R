4d1ebc1d5f5bf4cca7da70c88f60a3a552b4cff9
Rolf Simoes <rolfsimoes@gmail.com>
2018-03-17 11:37:45 -0300

    Update example scripts and files

--- inst/extdata/examples/costanza.R
+++ inst/extdata/examples/costanza.R
@@ -1,15 +1,10 @@
 require(raster)
 require(ggplot2)
-require(slideR)
+require(mrgf)
 
-# Costanza with slideR
-# 10x10
-f1 <- "~/Downloads/costanza.tif"
-f2 <- "~/Downloads/costanza2.tif"
-f2 <- f1
-# 4800x4800
-f1 = "~/Downloads/cerrado_h13v10_t01_h-all_v-all.tif"
-f2 = "~/Downloads/cerrado_h13v10_t02_h-all_v-all.tif"
+# Costanza with 10x10 images
+f1 <- system.file("extdata/tif/costanza1.tif", package = "mrgf")
+f2 <- system.file("extdata/tif/costanza2.tif", package = "mrgf")
 
 r1 <- raster::raster(f1)
 r2 <- raster::raster(f2)
@@ -22,7 +17,6 @@ m2 <- matrix(r2, nrow = nrow(r2))
 
 
 a <- costanza(m1, m2, resolution = 1:10)
-win2(1:25, dim = c(5,5), from = c(2,2), to = c(4, 4))
 
 ggplot(a) + 
     geom_line(aes(resolution, 
